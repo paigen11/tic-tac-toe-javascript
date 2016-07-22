4df118c00503a94b550e73531d5a81b12f186b1e
paigen11 <paigen11@gmail.com>
2016-07-22 09:40:25 -0400

    Testing branches in Github

--- scripts.js
+++ scripts.js
@@ -1,15 +1,11 @@
+// N Sized Version
+
 var whosTurn = 1; //start off on player 1's turn
 
-var winners = [
-	["A1", "A2", "A3"], //row 1
-	["B1", "B2", "B3"], //row 2
-	["C1", "C2", "C3"], //row 3
-	["A1", "B2", "C3"], //diag 1
-	["A1", "B1", "C1"], //col 1
-	["A2", "B2", "C2"], //col 2
-	["A3", "B3", "C3"], //col 3
-	["A3", "B2", "C1"]  //other diag
-];
+var winners = [];
+
+// 1. Build a winners array
+// 2. We need to populate the board
 
 
 var player1 = []; //array where we will stash the squares player 1 has checked
